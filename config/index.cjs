b75b0fab179bcb95834e17bcb631c9ae53daba4a
posterman1 <46317493+posterman1@users.noreply.github.com>
2023-08-13 18:15:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'rNNkS2GWfZVKTZUipVw1Htn6HDY5yXNdZF6jGsyLTjU',
+  CALLBACK_TEMPLATE_ID: '1gGg7wfxxBe5hYgAg3-4UddTD__iTa4kMdixMYCydhc',
 
   CALLBACK_USERS: [
     {
